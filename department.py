0ad99a072090506740254e0ffdcf2c51d9e83e92
Utsav Shrestha <utsav.shrestha@mavs.uta.edu>
2020-10-07 21:12:41 -0500

    Utsav

--- department.py
+++ department.py
@@ -7,13 +7,13 @@ connection = sqlite3.connect('department.db')
 c = connection.cursor()
 
 
-# Create a Table
-c.execute("""CREATE TABLE department (
-    dept_name text,
-    dept_num integer,
-    ph_num integer,
-    est_date text
-)
-""")
+# fetch the data
+c.execute("SELECT * FROM department")
+print(c.fetchall())
 
-# input data in a database table
+
+# Commit the command
+connection.commit()
+
+# Close the Connection
+connection.close()
